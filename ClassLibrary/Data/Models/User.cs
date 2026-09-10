ffc770fb225614d11796e4d1ee4dd18f65f09eee
ElisabethCreutz <elisabeth.creutz@gmail.com>
2026-09-10 12:33:33 +0200

    Added Usertable

---
+++ ClassLibrary/Data/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ClassLibrary.Data.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        [Required] [MaxLength(50)] public string UserName { get; set; }
+        [Required][MaxLength(20)] public string Password { get; set; }
+        [Required] public bool IsAdmin { get; set; }
+    }
+}
